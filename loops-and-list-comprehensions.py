498ba78eb43e4e1fef54dfe9340d740cddab4282
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-05-27 20:38:11 -0300

    finished loops and comprehensions

--- loops-and-list-comprehensions.py
+++ loops-and-list-comprehensions.py
@@ -5,3 +5,17 @@ print('\n')
 palavra = 'inconstitucionaliscimamente'
 for letra in palavra:
     print(letra, end='')
+print('\n')
+
+for i in range(4):
+    print(i, end=' ')
+
+palavra.upper()
+print(palavra.upper())
+
+print(sum([1 for letra in palavra if letra == 'i']))
+print(sum([letra == 'i' for letra in palavra]))
+
+print(any([False, True, False, False, False]))
+print(any([False, False, False, False, False]))
+print(any([letra == 'z' for letra in palavra]))
