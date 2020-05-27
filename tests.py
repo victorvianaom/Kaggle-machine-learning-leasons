875a248e86fbfd7d6e04c6f936d0a5a0c4409149
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-05-26 21:19:11 -0300

    some more python

--- tests.py
+++ tests.py
@@ -1,1 +1,10 @@
-print("a"*4)
+a = "a"
+a = a * 4
+print(a)
+print(type(a), type(1.1))
+print(5/3, 5//3)
+print(min(2, 55, 66, -9), max(2, 55, 66, -9))
+a = [2, 55, 66, -9]
+print(min(a), max(a))
+print((int("12")+1), (float("12.1")+1))
+print(type(int("12")+1), type(float("12.1")+1))
