16ce2571a64112cbe52d787cc199dc598a4dbdcf
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-05-31 12:58:36 -0300

    finished underfitting and overfitting

--- 02-Intro-To-Machine-Learning/underfitting-overfitting.py
+++ 02-Intro-To-Machine-Learning/underfitting-overfitting.py
@@ -14,8 +14,23 @@ X = melborn_data[features]
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=0)
 
 def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
-    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0) # NEW
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0) # balaceia o under com overfitting
     model.fit(train_X, train_y)
     preds_val = model.predict(val_X)
     mae = mean_absolute_error(val_y, preds_val)
     return mae
+
+leaf_set = [2, 3, 4, 5, 10, 20, 50, 100, 250, 500, 5000, 50000]
+mae_list = []
+for max_leaf_nodes in leaf_set:
+    mae_list.append(get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y))
+    #print("Max leaf nodes: %d \t\t Mean Absolute Error: %d" %(max_leaf_nodes, my_mae))
+print(mae_list)
+best_tree_size = leaf_set[mae_list.index(min(mae_list))]
+print('Best tree size: ', best_tree_size)
+
+## now I know the best_tree_size I should use all the data for my final model
+final_model = DecisionTreeRegressor(max_leaf_nodes=best_tree_size, random_state=0)
+final_model.fit(X, y)
+
+
