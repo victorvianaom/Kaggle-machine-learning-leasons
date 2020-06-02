afff82403cf283bb44ab86211ddb51c534452576
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-06-01 22:34:31 -0300

    in the middle of introduction, intermediate ML. I'M DONE FOR TODAY

--- 03-Intermediate-Machine-Learning/01-introduction.py
+++ 03-Intermediate-Machine-Learning/01-introduction.py
@@ -5,3 +5,18 @@
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
+
+path_train = os.path.join(os.path.dirname(__file__), 'data\\train.csv')
+path_test = os.path.join(os.path.dirname(__file__), 'data\\test.csv')
+X_full = pd.read_csv(path_train)
+X_test_full = pd.read_csv(path_test)
+
+y = X_full.SalePrice
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+
+X = X_full[features].copy() ##Modifications to the data or indices of the copy will not be reflected in the original object
+X_test = X_test_full[features].copy()
+
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+
+print(X_train.head())
