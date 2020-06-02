3c49ae54041ad2d2af89a31a743c48a81de94f7b
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-06-02 16:36:09 -0300

    finished introduction

--- 03-Intermediate-Machine-Learning/01-introduction.py
+++ 03-Intermediate-Machine-Learning/01-introduction.py
@@ -20,3 +20,27 @@ X_test = X_test_full[features].copy()
 X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
 
 print(X_train.head())
+#print(help(pd.DataFrame.copy))
+
+## defining various models:
+from sklearn.ensemble import RandomForestRegressor
+
+model_1 = RandomForestRegressor(n_estimators=50, random_state=0)
+model_2 = RandomForestRegressor(n_estimators=100, random_state=0)
+model_3 = RandomForestRegressor(n_estimators=100, criterion='mae', random_state=0)
+model_4 = RandomForestRegressor(n_estimators=200, min_samples_split=20, random_state=0)
+model_5 = RandomForestRegressor(n_estimators=100, max_depth=7, random_state=0)
+
+models = [model_1, model_2, model_3, model_4, model_5]
+
+## The best model will obtain the lowest MAE:
+from sklearn.metrics import mean_absolute_error
+
+def score_model(model, X_t=X_train, X_v=X_valid, y_t=y_train, y_v=y_valid):
+    model.fit(X_t, y_t)
+    preds = model.predict(X_v)
+    return mean_absolute_error(y_v, preds)
+
+for i in range(0, len(models)):
+    mae = score_model(models[i])
+    print("Model %d MAE: %d" % (i+1, mae))
